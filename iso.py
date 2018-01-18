debb876b9ceb623a3ee38d1457d6eceddab59520
kkranze <kkranze@sjnma.org>
2018-01-18 10:31:20 -0600

    final done

--- iso.py
+++ iso.py
@@ -2,6 +2,11 @@ number = input ("enter a number")
 
 def isogram(number):
 	for int in number:
+		if int == number.isdigit():
+			print ("true")
+		else:
+			print ("false")
+	for int in number:
 		if number.count(int) > 1:
 			return ("False")
 		else:
